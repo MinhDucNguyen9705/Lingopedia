7ac5fb0e50642f77f16ebded09d6ddc8d5df1d60
MinhDucNguyen9705 <nguyenminhduc9705@gmail.com>
2023-10-01 17:07:19 +0700

    update data processor

--- backend/database.py
+++ backend/database.py
@@ -1,6 +1,7 @@
 
 import sqlite3
 import json
+import pandas as pd
 
 # check_same_thread set to off
 connect = sqlite3.connect('lingopedia.db', check_same_thread=False)
@@ -20,38 +21,66 @@ def create_word(word,definition):
     """)
     connect.commit()
 
+# def find_word(word):
+#     cursor.execute(f"SELECT * FROM tu_dien WHERE tu == {word}")
+#     result = cursor.fetchall()
+#     # print(result)
+#     return result
+
 def find_word(word):
     cursor.execute(f"""
         SELECT * FROM tu_dien WHERE tu LIKE '%{word}%'
     """)
     result = cursor.fetchall()
-    print(result)
-    return result
+    # print(result)
+    return result[0][2]
 
-import sqlite3
-import json
+# import sqlite3
+# import json
 
-connect = sqlite3.connect('lingopedia.db')
-cursor = connect.cursor()
-cursor.execute("""
-        CREATE TABLE IF NOT EXISTS tu_dien (
-               id INTEGER PRIMARY KEY AUTOINCREMENT,
-               tu TEXT NOT NULL,
-               nghia TEXT NOT NULL
-               )
-""")
-connect.commit()
+# connect = sqlite3.connect('lingopedia.db')
+# cursor = connect.cursor()
+# cursor.execute("""
+#         CREATE TABLE IF NOT EXISTS tu_dien (
+#                id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                tu TEXT NOT NULL,
+#                nghia TEXT NOT NULL
+#                )
+# """)
+# connect.commit()
 
-def create_word(word,definition):
-    cursor.execute(f"""
-        INSERT INTO tu_dien (tu, nghia) VALUES ('{word}','{definition}')
-""")
+# def create_word(word,definition):
+#     cursor.execute(f"""
+#         INSERT INTO tu_dien (tu, nghia) VALUES ('{word}','{definition}')
+# """)
+#     connect.commit()
+
+# def find_word(word):
+#     cursor.execute(f"""
+#         SELECT * FROM tu_dien WHERE tu LIKE '%{word}%'
+# """)
+#     result = cursor.fetchall()
+#     # print(result)
+#     return result
+
+def csv_read(file):
+    df = pd.read_csv(file)
+    tu_list = list(df["tu"])
+    nghia_list = list(df["nghia"])
+    for i in range (len(tu_list)):
+        cursor.execute("INSERT INTO tu_dien (tu, nghia) VALUES (?, ?)",(tu_list[i], nghia_list[i]))
+        connect.commit()
+    return "Them thanh cong"
+
+def take_all_word():
+    cursor.execute("SELECT tu FROM tu_dien")
     connect.commit()
+    list_of_words = cursor.fetchall()
+    lst = []
+    for i in range (len(list_of_words)):
+        lst.append(list_of_words[i][0])
+    return lst
 
-def find_word(word):
-    cursor.execute(f"""
-        SELECT * FROM tu_dien WHERE tu LIKE '%{word}%'
-""")
-    result = cursor.fetchall()
-    print(result)
-    return result
+# print(csv_read("data.csv"))
+print(find_word("allergen"))
+# print(take_all_word())
--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,11 @@
 from typing import Union
 from fastapi import FastAPI
-from database import create_word, find_word
+from database import create_word, find_word, take_all_word
 
 app = FastAPI()
+
+word_list = [word for word in take_all_word()]
+# meaning_list = [find_word(word) for word in take_all_word()]
 
 @app.get("/")
 def read_root():
@@ -10,7 +13,11 @@ def read_root():
 
 @app.get("/find_word/{word}")
 async def get_word(word :str):
-    return find_word(word)
+    return [word,find_word(word)]
+
+@app.get("/all_words/")
+async def all_words():
+    return word_list
 
 @app.post("/create_word")
 async def post_word(new_word :dict):
