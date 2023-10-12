5a046107bff0df3286a5b7c71b563f3ed55e2ddd
MinhDucNguyen9705 <nguyenminhduc9705@gmail.com>
2023-10-12 20:40:33 +0700

    test

--- backend/test.py
+++ backend/test.py
@@ -1,13 +1,14 @@
 import requests
 url = "https://lingopedia.vercel.app"
 word = input()
-# meaning = input()
-# topic = input()
-# info = {'tu': "", 'nghia': "", "chu_de" : ""}
-# info['tu'] = word
-# info['nghia']= meaning
-# info['chu_de'] = topic
-# print(info)
-# res = requests.post(f"{url}/find_word/", json = info)
+meaning = input()
+topic = input()
+info = {'tu': "", 'nghia': "", "chu_de" : ""}
+info['tu'] = word
+info['nghia']= meaning
+info['chu_de'] = topic
+print(info)
+res = requests.post(f"{url}/find_word/", json = info)
+print("1")
 traloi = requests.get(f'{url}/find_word/{word}')
 print(traloi.text)
